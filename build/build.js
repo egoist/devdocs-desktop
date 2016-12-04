15ecec7fd90e5a56c0b4ec0cac5ad9d4ca34af94
EGOIST <0x142857@gmail.com>
2016-12-04 21:40:27 +0800

    tweak build script

--- build/build.js
+++ build/build.js
@@ -26,7 +26,7 @@ const cb = (err, paths) => {
 
 platforms.macos = () => {
   $.rm('./app/yarn.lock')
-  $.rm('-rf', './dist')
+  $.rm('-rf', './dist/DevDocs-darwin-x64')
   $.mkdir('-p', './dist/installers')
   packager(Object.assign({}, defaults, {
     platform: 'darwin',
