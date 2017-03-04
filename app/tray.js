738097682c4b5cbd4c9b2d7c6f2ca5ef141a279f
EGOIST <0x142857@gmail.com>
2017-03-04 22:33:16 +0800

    add tray for windows and linux

---
+++ app/tray.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const electron = require('electron')
+
+const app = electron.app
+let tray = null
+
+exports.create = win => {
+  if (process.platform === 'darwin' || tray) {
+    return
+  }
+
+  const iconPath = path.join(__dirname, 'static/tray.png')
+
+  const toggleWin = () => {
+    if (win.isVisible()) {
+      win.hide()
+    } else {
+      win.show()
+    }
+  }
+
+  const contextMenu = electron.Menu.buildFromTemplate([
+    {
+      label: 'Toggle',
+      click() {
+        toggleWin()
+      }
+    },
+    {
+      type: 'separator'
+    },
+    {
+      role: 'quit'
+    }
+  ])
+
+  tray = new electron.Tray(iconPath)
+  tray.setToolTip(`${app.getName()}`)
+  tray.setContextMenu(contextMenu)
+  tray.on('click', toggleWin)
+}
