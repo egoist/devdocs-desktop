bef8313ccadda3e55e6878814aff71b8df8077ee
EGOIST <0x142857@gmail.com>
2017-06-14 00:44:05 +0800

    better searcher styling

--- app/renderer/searcher.js
+++ app/renderer/searcher.js
@@ -24,11 +24,23 @@ module.exports = class Searcher {
     const $wrapper = document.createElement('div')
     $wrapper.innerHTML = `
     <div class="searcher searcher__hidden">
-      <input class="searcher-input" placeholder="type..." />
+      <input class="searcher-input" placeholder="Search..." />
       <span class="searcher-progress searcher-progress__disabled"></span>
-      <button class="searcher-prev">↑</button>
-      <button class="searcher-next">↓</button>
-      <button class="searcher-close">✕</button>
+      <button class="searcher-action searcher-prev">
+        <svg id="i-chevron-top" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+          <path d="M30 20 L16 8 2 20" />
+        </svg>
+      </button>
+      <button class="searcher-action searcher-next">
+        <svg id="i-chevron-bottom" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+          <path d="M30 12 L16 24 2 12" />
+        </svg>
+      </button>
+      <button class="searcher-action searcher-close">
+        <svg id="i-close" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+          <path d="M2 30 L30 2 M30 30 L2 2" />
+        </svg>
+      </button>
     </div>
     `
     document.body.appendChild($wrapper)
@@ -75,6 +87,7 @@ module.exports = class Searcher {
   findPrev(value, opts) {
     if (value) {
       this.target.findInPage(
+        value,
         Object.assign(
           {
             forward: false
