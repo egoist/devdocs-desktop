093add9b67a841dd33af3a47a95ca443299fc177
EGOIST <0x142857@gmail.com>
2017-06-13 21:32:42 +0800

    add context menu to searcher

--- app/renderer/main.js
+++ app/renderer/main.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const { ipcRenderer, remote, shell } = require('electron')
 const mkdirp = require('mkdirp')
+const contextMenu = require('electron-context-menu')
 const searchInPage = require('electron-in-page-search').default
 const { configDir } = require('../utils')
 
@@ -27,24 +28,30 @@ webview.src = 'https://devdocs.io'
 webview.preload = configDir('custom.js')
 document.body.appendChild(webview)
 
-webview.addEventListener('dom-ready', () => {
-  // Insert custom css
-  webview.insertCSS(fs.readFileSync(configDir('custom.css'), 'utf8'))
-  // Add context menu to webview
-  require('electron-context-menu')({
-    window: webview,
-    showInspectElement: true
-  })
-})
-
-// Listen event for toggling search bar
+// Initialize in-page searcher
 const inPageSearch = searchInPage(webview)
+
 ipcRenderer.on('toggle-search', () => {
   if (inPageSearch.opened) {
     inPageSearch.closeSearchWindow()
   } else {
     inPageSearch.openSearchWindow()
   }
+})
+
+webview.addEventListener('dom-ready', () => {
+  inPageSearch.initialize()
+  // Insert custom css
+  webview.insertCSS(fs.readFileSync(configDir('custom.css'), 'utf8'))
+  // Add context menus
+  contextMenu({
+    window: webview,
+    showInspectElement: true
+  })
+  contextMenu({
+    window: inPageSearch.searcher,
+    showInspectElement: true
+  })
 })
 
 // Update app title
