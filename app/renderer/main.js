7ffd4410bcb021a6c5b87e13170f89f2858b8137
EGOIST <0x142857@gmail.com>
2017-06-14 14:27:10 +0800

    use custom titlebar on macos

--- app/renderer/main.js
+++ app/renderer/main.js
@@ -1,10 +1,12 @@
 const os = require('os')
+const path = require('path')
 const fs = require('fs')
 const { ipcRenderer, remote, shell } = require('electron')
 const mkdirp = require('mkdirp')
 const contextMenu = require('electron-context-menu')
 const { configDir } = require('../utils')
 const Searcher = require('./searcher')
+const config = require('../config')
 
 const win = remote.getCurrentWindow()
 
@@ -21,12 +23,11 @@ function ensureCustomFiles() {
 }
 
 function createWebView() {
-  console.log('init webview')
   // Create webview
   const webview = document.createElement('webview')
   webview.className = 'webview'
   webview.src = 'https://devdocs.io'
-  webview.preload = `file://${configDir('custom.js')}`
+  webview.preload = `file://${path.join(__dirname, 'preload.js')}`
   document.body.appendChild(webview)
 
   // Initialize in-page searcher
@@ -36,9 +37,22 @@ function createWebView() {
     searcher.toggle()
   })
 
+  webview.addEventListener('ipc-message', e => {
+    if (e.channel === 'switch-mode') {
+      const [mode] = e.args
+      config.set('mode', mode)
+      if (mode === 'dark') {
+        document.body.classList.add(`is-dark-mode`)
+      } else {
+        document.body.classList.remove(`is-dark-mode`)
+      }
+    }
+  })
+
   webview.addEventListener('dom-ready', () => {
     // Insert custom css
     webview.insertCSS(fs.readFileSync(configDir('custom.css'), 'utf8'))
+    webview.executeJavaScript(fs.readFileSync(configDir('custom.js'), 'utf8'))
     // Add context menus
     contextMenu({
       window: webview,
@@ -62,4 +76,8 @@ function createWebView() {
 ensureCustomFiles()
 createWebView()
 
-document.body.className = `is-${os.platform()}`
+document.body.classList.add(`is-${os.platform()}`)
+
+if (config.get('mode') === 'dark') {
+  document.body.classList.add('is-dark-mode')
+}
