b3d530a91d99be4d07cf580ec6c036ae186bde61
EGOIST <0x142857@gmail.com>
2020-03-07 18:47:03 +0800

    fix lint

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
-const { notarize } = require('electron-notarize');
+require('dotenv').config()
+const { notarize } = require('electron-notarize')
 
-exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;
+module.exports = context => {
+  const { electronPlatformName, appOutDir } = context
   if (electronPlatformName !== 'darwin') {
-    return;
+    return
   }
 
-  const appName = context.packager.appInfo.productFilename;
+  const appName = context.packager.appInfo.productFilename
 
-  return await notarize({
+  return notarize({
     appBundleId: 'sh.egoist.devdocs',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
-  });
-};
+    appleIdPassword: process.env.APPLEIDPASS
+  })
+}
